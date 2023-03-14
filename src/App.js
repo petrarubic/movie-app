322eec2a685c3f1a36e048c9c648af7b1f55efdf
petrarubic <petra@caelor.com>
2023-03-14 11:12:02 +0100

    add routes and define pages

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
+import { Route, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar/Navbar";
+import Discover from "./pages/Discover";
+import Home from "./pages/Home";
+import Movie from "./pages/Movie";
+import PageNotFound from "./pages/PageNotFound";
+
 function App() {
-  return <div>Hello World</div>;
+  return (
+    <div>
+      <Navbar />
+      <div style={{ marginTop: "40px" }}>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/discover-movies">
+            <Route index element={<Discover />} />
+            <Route path=":id" element={<Movie />} />
+          </Route>
+          <Route path="*" element={<PageNotFound />} />
+        </Routes>
+      </div>
+    </div>
+  );
 }
 
 export default App;
---
+++ src/pages/Discover.jsx
@@ -0,0 +1,5 @@
+const Discover = () => {
+  return <div>Discover movies</div>;
+};
+
+export default Discover;
