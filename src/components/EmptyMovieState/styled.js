89683cecb401be35cc6fd5a4b9edea6099148bd0
petrarubic <petra@caelor.com>
2023-04-05 20:33:00 +0200

    add movie details page and implement favorites helper

---
+++ src/components/EmptyMovieState/styled.js
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+export const EmptyMovieStateWrapper = styled.div`
+  grid-column: 1/-1;
+  display: flex;
+  width: 100%;
+  height: 100%;
+  flex-direction: column;
+  align-items: center;
+  margin-top: 50px;
+  gap: 50px;
+`;
+
+export const EmptyMovieStateHeading = styled.h1`
+  text-align: center;
+  font-size: 24px;
+`;
